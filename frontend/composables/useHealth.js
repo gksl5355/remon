32ddc2d4d4b32cdb4d27316359166e86e89b6c38
SunYoung710 <tdrubytd@gmail.com>
2025-11-12 11:02:18 +0900

    AdminPage and pyproject.toml Update

--- frontend/composables/useHealth.js
+++
@@ -1,8 +0,0 @@
-import { ref } from 'vue'
-import { checkHealthAPI } from '../services/api.js'
-
-export function useHealth() {
-  const status = ref('')
-  const refresh = async () => { status.value = (await checkHealthAPI()).status }
-  return { status, refresh }
-}
